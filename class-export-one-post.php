450207964d2b420945444474fd524e8afb63919f
Ben Leivian <bleivian@gmail.com>
2020-05-27 14:14:44 -0700

    Add wpnonce.

--- class-export-one-post.php
+++ class-export-one-post.php
@@ -54,9 +54,12 @@ class Export_One_Post {
 				$export_url = add_query_arg( array(
 					'download'      => '',
 					'export_single' => get_the_ID(),
+					'_wpnonce' => wp_create_nonce( 'export_single' ),
 				), admin_url( 'export.php' ) );
 			?>
-			<a href="<?php echo esc_url( $export_url ); ?>"><?php esc_html_e( 'Export This', 'export-one-post' ); ?></a>
+			<a href="<?php echo esc_url( $export_url ); ?>">
+				<?php esc_html_e( 'Export XML', 'export-one-post' ); ?>
+			</a>
 		</div><?php
 	}
 
@@ -69,14 +72,14 @@ class Export_One_Post {
 	 */
 	function export_args( $args ) {
 		// if no export_single var, it's a normal export - don't interfere
-		if ( ! isset( $_GET['export_single'] ) ) {
-			return $args;
-		}
+		if ( isset( $_GET['export_single'] ) ) {
+			check_admin_referer('export_single');
 
-		// use our fake date so the query is easy to find (because we don't have a good hook to use)
-		$args['content']    = 'post';
-		$args['start_date'] = $this->fake_date;
-		$args['end_date']   = $this->fake_date;
+			// use our fake date so the query is easy to find (because we don't have a good hook to use)
+			$args['content']    = 'post';
+			$args['start_date'] = $this->fake_date;
+			$args['end_date']   = $this->fake_date;
+		}
 
 		return $args;
 	}
@@ -89,32 +92,32 @@ class Export_One_Post {
 	 * @return string Modified SQL query
 	 */
 	function query( $query ) {
-		if ( ! isset( $_GET['export_single'] ) ) {
-			return $query;
+		if (isset($_GET['export_single'])) {
+				check_admin_referer('export_single');
+
+				global $wpdb;
+
+				// This is the query WP will build (given our arg filtering above)
+				// Since the current_filter isn't narrow, we'll check each query
+				// to see if it matches, then if it is we replace it
+				// @see https://github.com/wordpress/wordpress/blob/5.4.1/wp-admin/includes/export.php#L144
+				$test = $wpdb->prepare(
+						"SELECT ID FROM {$wpdb->posts}  WHERE {$wpdb->posts}.post_type = 'post' AND {$wpdb->posts}.post_status != 'auto-draft' AND {$wpdb->posts}.post_date >= %s AND {$wpdb->posts}.post_date < %s",
+						date('Y-m-d', strtotime($this->fake_date)),
+						date('Y-m-d', strtotime('+1 month', strtotime($this->fake_date)))
+				);
+
+				if ($test !== $query) {
+						return $query;
+				}
+
+				// divide query
+				$split    = explode('WHERE', $query);
+				// replace WHERE clause
+				$split[1] = $wpdb->prepare(" {$wpdb->posts}.ID = %d", intval($_GET['export_single']));
+				// put query back together
+				$query    = implode('WHERE', $split);
 		}
-
-		global $wpdb;
-
-		// This is the query WP will build (given our arg filtering above)
-		// Since the current_filter isn't narrow, we'll check each query
-		// to see if it matches, then if it is we replace it
-		// @see https://github.com/wordpress/wordpress/blob/5.4.1/wp-admin/includes/export.php#L144
-		$test = $wpdb->prepare(
-			"SELECT ID FROM {$wpdb->posts}  WHERE {$wpdb->posts}.post_type = 'post' AND {$wpdb->posts}.post_status != 'auto-draft' AND {$wpdb->posts}.post_date >= %s AND {$wpdb->posts}.post_date < %s",
-			date( 'Y-m-d', strtotime( $this->fake_date ) ),
-			date( 'Y-m-d', strtotime( '+1 month', strtotime( $this->fake_date ) ) )
-		);
-
-		if ( $test !== $query ) {
-			return $query;
-		}
-
-		// divide query
-		$split    = explode( 'WHERE', $query );
-		// replace WHERE clause
-		$split[1] = $wpdb->prepare( " {$wpdb->posts}.ID = %d", intval( $_GET['export_single'] ) );
-		// put query back together
-		$query    = implode( 'WHERE', $split );
 
 		return $query;
 	}
